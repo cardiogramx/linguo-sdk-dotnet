2753437bfff193f5e78cec19dfb77a68608d5d09
cardiogram <felix@abi-ng.com>
2020-07-14 04:50:04 +0100

    modified the ttsclient for perf

--- Clients/TextToSpeechClient.cs
+++ Clients/TextToSpeechClient.cs
@@ -7,7 +7,7 @@ namespace Linguo
     public partial class LinguoClient
     {
         public async ValueTask<byte[]> GetAudioFromTextAsync(TextToSpeechModel model, CancellationToken cancellationToken = default) =>
-            await _httpClient.PostAsync<byte[]>("/api/v1/Speech/GetAudioFromText", model, cancellationToken);
+            await _httpClient.PostAsync("/api/v1/Speech/GetAudioFromText", model, cancellationToken);
 
         public async ValueTask DownloadAudioAsync(TextToSpeechModel model, string filePath, CancellationToken cancellationToken = default) =>
             await File.WriteAllBytesAsync(filePath, await GetAudioFromTextAsync(model, cancellationToken), cancellationToken);
